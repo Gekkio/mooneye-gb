a9fb0c75be9e2fd694995c2fae824aea5d51bf02
Joonas Javanainen <joonas.javanainen@gmail.com>
2015-01-10 00:12:12 +0200

    Improve boot_div2 to work with GBC

--- tests/boot_div2/test.s
+++ tests/boot_div2/test.s
@@ -2,19 +2,24 @@
 .include "common.i"
 
   ld_a_ff DIV
-  ld c, a
+  ld d, a
   nops 2
   ld_a_ff DIV
-  ld b, a
+  ld c, a
   nops 15
+  ld_a_ff DIV
+  ld b, a
   ld_a_ff DIV
 
   ; GBP MGB-001 (probably DMG as well):
-  ; A, B should contain $AC
-  ; C should contain $AB
-  ; GBASP AGS-101 (probably GBC as well):
+  ; A, B, C should contain $AC
+  ; D should contain $AB
+  ; GBASP AGS-101 (probably GBA as well):
+  ; A, B should contain $27
+  ; C, D should contain $26
+  ; GBC CGB-001
   ; A should contain $27
-  ; B, C should contain $26
+  ; B, C, D should contain $26
 
 test_finish:
   save_results
