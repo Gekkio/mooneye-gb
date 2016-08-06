cba19a2b5dae57df98bccd4aac531170b2febfeb
Joonas Javanainen <joonas.javanainen@gmail.com>
2016-08-06 21:03:16 +0300

    Reduce test parallelism in Travis

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+CARGO_FLAGS=""
+
+if [ -n "${CI}" ]; then
+    CARGO_FLAGS="-j 2"
+fi
+
 make WLAFLAGS="-DACCEPTANCE_TEST=1" -C tests clean all
-cargo test --features acceptance_tests
+cargo test --features acceptance_tests ${CARGO_FLAGS}
