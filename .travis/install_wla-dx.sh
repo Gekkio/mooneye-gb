4ef452ae70b423152927874bb9147529c37dbb9c
Joonas Javanainen <joonas.javanainen@gmail.com>
2016-12-28 17:25:56 +0200

    Oops

--- .travis/install_wla-dx.sh
+++ .travis/install_wla-dx.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-WLA_DX_COMMIT="d1dcfd6142a71702471580e6ddca24652a63d00f"
+CACHED_VERSION=`cat ${HOME}/bin/wla-dx.version || true`
+
+if [ "${CACHED_VERSION}" = "${WLA_DX_COMMIT}" ]; then
+  echo "Using cached wla-dx binaries"
+  exit 0
+fi
 
 curl -sSL "https://github.com/vhelin/wla-dx/archive/${WLA_DX_COMMIT}.tar.gz" | tar xzv -C "${HOME}"
 cd "${HOME}/wla-dx-${WLA_DX_COMMIT}"
