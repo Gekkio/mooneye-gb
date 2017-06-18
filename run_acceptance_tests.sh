0f8e665b075b8e711b1e2f520db3b5c9ad4c01bc
Joonas Javanainen <joonas.javanainen@gmail.com>
2017-06-18 10:57:56 +0300

    Run acceptance tests in parallel, compile in release mode

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+JOBS="-j `nproc`"
 CARGO_FLAGS=""
 
 if [ -n "${CI}" ]; then
-    CARGO_FLAGS="-j 1"
+    CARGO_FLAGS="${JOBS} --release"
 fi
 
-make WLAFLAGS="-DACCEPTANCE_TEST=1" -C tests clean all
+make -C tests clean
+make ${JOBS} WLAFLAGS="-DACCEPTANCE_TEST=1" -C tests all
 cargo test --features acceptance_tests ${CARGO_FLAGS}
