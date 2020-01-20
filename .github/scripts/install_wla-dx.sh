3afa3acb00aa7db4534acb3e2c2f50c9cf99d9e3
Joonas Javanainen <joonas.javanainen@gmail.com>
2020-01-20 18:14:26 +0200

    Attempt 2

--- .github/scripts/install_wla-dx.sh
+++ .github/scripts/install_wla-dx.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-CACHED_VERSION=`cat ${HOME}/bin/wla-dx.version || true`
-
-if [ "${CACHED_VERSION}" = "${WLA_DX_COMMIT}" ]; then
-  echo "Using cached wla-dx binaries"
-  exit 0
-fi
-
 curl -sSL "https://github.com/${WLA_DX_REPO}/archive/${WLA_DX_COMMIT}.tar.gz" | tar xzv -C "${HOME}"
-cd "${HOME}/wla-dx-${WLA_DX_COMMIT}"
+mv "${HOME}/wla-dx-${WLA_DX_COMMIT}" "${HOME}/wla-dx"
+cd "${HOME}/wla-dx"
 cmake .
 make
-
-mkdir -p "${HOME}/bin"
-cp binaries/wla-gb binaries/wlalink "${HOME}/bin"
-echo -n "${WLA_DX_COMMIT}" > "${HOME}/bin/wla-dx.version"
